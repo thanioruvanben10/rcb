427989e90ab4dd45cd0f8cb6a4e1de9d510a59c2
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-05-30 02:29:25 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -9,11 +9,10 @@ anchor url should look like:
 https://www.google.com/recaptcha/api2/anchor?ar=1&k=...
 
 '''
-new = input("1 = ")
-ANCHOR_URL = new.replace('amp;','')
+ANCHOR_URL = "https://www.google.com/recaptcha/api2/anchor?ar=1&k=6Lcr1ncUAAAAAH3cghg6cOTPGARa8adOf-y9zv2x&co=aHR0cHM6Ly9vdW8uaW86NDQz&hl=en&v=1B_yv3CBEV10KtI2HJ6eEXhJ&size=invisible&cb=4xnsug1vufyr"
 print ("🥰 : ",ANCHOR_URL)
 def RecaptchaV3(ANCHOR_URL):
-    url_base = 'https://www.recaptcha.net/recaptcha/'
+    url_base = 'https://www.google.com/recaptcha/'
     post_data = "v={}&reason=q&c={}&k={}&co={}"
     
     client = requests.Session()
