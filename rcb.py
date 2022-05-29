a8fdc919343159be6ec4452be6a1bf4a49eeb2be
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-05-30 02:13:36 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -9,9 +9,9 @@ anchor url should look like:
 https://www.google.com/recaptcha/api2/anchor?ar=1&k=...
 
 '''
-ANCHOR_URL = input("1 = ")
-ANCHOR_URL = ANCHOR_URL.replace('amp;','')
-
+new = input("1 = ")
+ANCHOR_URL = new.replace('amp;','')
+print ("🥰 : ",ANCHOR_URL)
 def RecaptchaV3(ANCHOR_URL):
     url_base = 'https://www.recaptcha.net/recaptcha/'
     post_data = "v={}&reason=q&c={}&k={}&co={}"
@@ -23,18 +23,19 @@ def RecaptchaV3(ANCHOR_URL):
     })
     
     matches = re.findall('([api2|enterprise]+)\/anchor\?(.*)', ANCHOR_URL)[0]
+    print ("\n matches = ",matches)
     url_base += matches[0]+'/'
     params = matches[1]
-    
     res = client.get(url_base+'anchor', params=params)
+    print ("\n res 1 = ",res)
     token = re.findall(r'"recaptcha-token" value="(.*?)"', res.text)[0]
-    
+    print ("\n token = ",token)
     params = dict(pair.split('=') for pair in params.split('&'))
+    print ("\n params = ",res)
     post_data = post_data.format(params["v"], token, params["k"], params["co"])
-    
+    print ("\n post_data = ", post_data)
     res = client.post(url_base+'reload', params=f'k={params["k"]}', data=post_data)
-    
-    print ("\n👉",res)
+    print ("\n res2 = ",res)
     
 # -------------------------------------------
 
