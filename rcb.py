a51e41e944f9604b3bfe7b400a23b3ba34976627
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-05-30 01:51:50 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -10,24 +10,6 @@ https://www.google.com/recaptcha/api2/anchor?ar=1&k=...
 
 '''
 ANCHOR_URL = input("1 = ")
+ANCHOR_URL = ANCHOR_URL.replace('amp;','')
+print (ANCHOR_URL)
 
-def RecaptchaV3(ANCHOR_URL):
-    url_base = "https://www.google.com/recaptcha/"
-    post_data = "v={}&reason=q&c={}&k={}&co={}"
-    client = requests.Session()
-    client.headers.update({
-        'content-type': 'application/x-www-form-urlencoded'
-    })
-    matches = re.findall('([api2|enterprise]+)\/anchor\?(.*)', ANCHOR_URL)[0]
-    url_base += matches[0]+'/'
-    params = matches[1]
-    res = client.get(url_base+'anchor', params=params)
-    print(res)
-    
-# -------------------------------------------
-
-ans = RecaptchaV3(ANCHOR_URL)
-
-print(ans)
-
-# If you are not sure how/where to use this token then this script is not for you :)
