1eab32caa0e7b4e62d12b4b7ed1aa954358159e4
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-05-30 01:00:49 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -9,30 +9,25 @@ anchor url should look like:
 https://www.google.com/recaptcha/api2/anchor?ar=1&k=...
 
 '''
-
-ANCHOR_URL = input ("Enter : ")
-
-# -------------------------------------------
+ANCHOR_URL = 'https://www.google.com/recaptcha/api2/anchor?ar=1&k=6Lcr1ncUAAAAAH3cghg6cOTPGARa8adOf-y9zv2x&co=aHR0cHM6Ly9vdW8uaW86NDQz&hl=en&v=1B_yv3CBEV10KtI2HJ6eEXhJ&size=invisible&cb=4xnsug1vufyr'
 
 def RecaptchaV3(ANCHOR_URL):
-    url_base = 'https://www.recaptcha.net/recaptcha/'
+    url_base = 'https://www.google.com/recaptcha/'
     post_data = "v={}&reason=q&c={}&k={}&co={}"
-    
     client = requests.Session()
-    
     client.headers.update({
         'content-type': 'application/x-www-form-urlencoded'
     })
-    
     matches = re.findall('([api2|enterprise]+)\/anchor\?(.*)', ANCHOR_URL)[0]
-    print(matches)
     url_base += matches[0]+'/'
     params = matches[1]
-    
     res = client.get(url_base+'anchor', params=params)
-    print(res)
-    token = re.findall(r'"recaptcha-token" value="(.*?)"', res.text)
-    print(token)
+    token = re.findall(r'"recaptcha-token" value="(.*?)"', res.text)[0]
+    params = dict(pair.split('=') for pair in params.split('&'))
+    post_data = post_data.format(params["v"], token, params["k"], params["co"])
+    res = client.post(url_base+'reload', params=f'k={params["k"]}', data=post_data)
+    answer = re.findall(r'"rresp","(.*?)"', res.text)[0]    
+    return answer
     
 # -------------------------------------------
 
