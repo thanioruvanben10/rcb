fc9e61bbf2f952ff41326bba090abaca98d2d8c9
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-07-20 14:43:03 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -3,7 +3,7 @@ import requests
 url = input("Enter link: ")
 password = input("Enter PWD: ")
 Login = 'Login'
-r = requests.post(url, allow_redirects=True, data={
+r = requests.post(url, data={
     'password': password,
     })
 print(r)
