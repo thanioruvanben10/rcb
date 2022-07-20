bee8b3d167ee654b02d4cbbef4937582fa598c19
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-07-20 14:32:49 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -1,14 +1,10 @@
 import requests
-import cloudscraper
-from bs4 import BeautifulSoup
-import re
-import time
 
-
-a13=input("Enter Mx player url here: ")
-x = int(input())
-y = int(input())
-for x in range(x,y+1):
-   a11=a13+str(x)
-   x+=1
-   print(a11)
+url = input("Enter link: ")
+password = input("Enter PWD: ")
+Login = 'Login'
+r = requests.post(url, allow_redirects=True, data={
+    'password': password,
+    'Login': Login
+    })
+print(r)
