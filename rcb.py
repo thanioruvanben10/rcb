91c0792e54ccfad86bb766370b73341fbde1cc01
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-07-21 22:34:48 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -1,7 +1,45 @@
-import requests
-url = input ()
-values = {'user': 'admin',
-          'password': 'joinstreamhub'}
+import time
+import cloudscraper
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
-r = requests.post(url, data=values)
-print (r.content)
+# eg: https://gplinks.co/XXXX
+url = input()
+
+# =======================================
+
+def gplinks_bypass(url: str):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    p = urlparse(url)
+    final_url = f'{p.scheme}://{p.netloc}/links/go'
+
+    res = client.head(url)
+    print (res.content)
+    header_loc = res.headers['location']
+    param = header_loc.split('postid=')[-1]
+    req_url = f'{p.scheme}://{p.netloc}/{param}'
+
+    p = urlparse(header_loc)
+    ref_url = f'{p.scheme}://{p.netloc}/'
+
+    h = { 'referer': ref_url }
+    res = client.get(req_url, headers=h, allow_redirects=False)
+    print ("\n\n\n",res.content)
+    bs4 = BeautifulSoup(res.content, 'html.parser')
+    print ("\n\n\n",bs4)
+    inputs = bs4.find_all('input')
+    data = { input.get('name'): input.get('value') for input in inputs }
+
+    h = {
+        'referer': ref_url,
+        'x-requested-with': 'XMLHttpRequest',
+    }
+    time.sleep(10)
+    res = client.post(final_url, headers=h, data=data)
+    try:
+        return res.json()['url'].replace('\/','/')
+    except: return 'Something went wrong :('
+
+# =======================================
+
+print(gplinks_bypass(url))
