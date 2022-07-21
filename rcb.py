7328a3cdc029a4ed516e365b709a0a5e54004fa1
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-07-21 23:48:52 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -37,7 +37,7 @@ def gplinks_bypass(url: str):
     print ("\n\n\n",final_url)
     print ("\n\n\n",data)
     x=req_url
-    a1=scraper.get(x).text
+    a1=client.get(x).text
     soup4=BeautifulSoup(a1,'html.parser')
     print("\n\n New Content \n\n",soup4)
     a11=soup4.find_all(src,text="reCAPTCHA")
