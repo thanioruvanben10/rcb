eb8ecea4dac58a632067bea7a31724e2e90205bf
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-07-22 06:51:05 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -7,6 +7,25 @@ from bs4 import BeautifulSoup
 url = "https://gplinks.co/yXS5"
 
 # =======================================
+ANCHOR_URL = "https://www.recaptcha.net/recaptcha/api2/anchor?ar=1&k=6LezO6kUAAAAAGAYpazfxwlPpBVljyiMIhj0kLA7&co=aHR0cHM6Ly93d3cuY2FzYWRhYmViaWRhLmNvbS5icjo0NDM.&hl=pt-BR&v=4rwLQsl5N_ccppoTAwwwMrEN&size=invisible&cb=vvcfcwwb7aum"
+
+def RecaptchaV3(ANCHOR_URL):
+    url_base = 'https://www.recaptcha.net/recaptcha/'
+    post_data = "v={}&reason=q&c={}&k={}&co={}"
+    client = requests.Session()
+    client.headers.update({
+        'content-type': 'application/x-www-form-urlencoded'
+    })
+    matches = re.findall('([api2|enterprise]+)\/anchor\?(.*)', ANCHOR_URL)[0]
+    url_base += matches[0]+'/'
+    params = matches[1]
+    res = client.get(url_base+'anchor', params=params)
+    token = re.findall(r'"recaptcha-token" value="(.*?)"', res.text)[0]
+    params = dict(pair.split('=') for pair in params.split('&'))
+    post_data = post_data.format(params["v"], token, params["k"], params["co"])
+    res = client.post(url_base+'reload', params=f'k={params["k"]}', data=post_data)
+    answer = re.findall(r'"rresp","(.*?)"', res.text)[0]
+    return answer
 
 def gplinks_bypass(url: str):
     client = cloudscraper.create_scraper(allow_brotli=False)
@@ -25,11 +44,10 @@ def gplinks_bypass(url: str):
     res = client.get(req_url, headers=h, allow_redirects=False)
 
     bs4 = BeautifulSoup(res.content, 'html.parser')
-    print(bs4)
     inputs = bs4.find_all('input')
-    inputs2 = bs4.find_all("a",href=True)
     data = { input.get('name'): input.get('value') for input in inputs }
-
+    ans = RecaptchaV3(ANCHOR_URL)
+    data['_Token[unlocked]']=ans
     h = {
         'referer': ref_url,
         'x-requested-with': 'XMLHttpRequest',
