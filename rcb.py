2c59ef9425fe0a8c3cbf7fde672694b937778b45
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-07-29 14:05:37 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -5,7 +5,6 @@ hijk = input("enter your link")
 scraper = cloudscraper.create_scraper(allow_brotli=False)
 lmno=scraper.get(hijk).text
 soup4=BeautifulSoup(lmno,'html.parser')
-print (soup4)
 for pqrs in soup4.find_all('a',href=True):
     mag_reg= r"magnet:\?xt=urn:btih:[a-zA-Z0-9]*"
     tyvw=mag_reg.findall(pqrs)
