0ce65d75ac0c4ccb5730a8ebb826f60956c3583b
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-07-29 12:49:21 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -6,6 +6,6 @@ scraper = cloudscraper.create_scraper(allow_brotli=False)
 lmno=scraper.get(hijk).text
 soup4=BeautifulSoup(lmno,'html.parser')
 print (soup4)
-for pqrs in soup4.find_all(href=True):
+for pqrs in soup4.find_all(href=True,alt="magnet.png"):
     tuvw =pqrs.get('href')
-    print("/Qbmirror +",tuvw)
+    print("/Qbmirror ",tuvw)
