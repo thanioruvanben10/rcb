85c2376530230285ead95372a60b430edd49e97c
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-07-29 14:10:01 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -1,11 +1,13 @@
 import cloudscraper
 from bs4 import BeautifulSoup
+import re
 
 hijk = input("enter your link")
 scraper = cloudscraper.create_scraper(allow_brotli=False)
 lmno=scraper.get(hijk).text
 soup4=BeautifulSoup(lmno,'html.parser')
+mag=re.compile("magnet:\?xt=urn:btih:[a-zA-Z0-9]*")
 for pqrs in soup4.find_all('a',href=True):
-    mag_reg= r"magnet:\?xt=urn:btih:[a-zA-Z0-9]*"
-    tyvw=mag_reg.findall(pqrs)
-    print("/Qbmirror ",tuvw)
+    j=str(pqrs)
+    x=mag.findall(j)
+    print(x)
