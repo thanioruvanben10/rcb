ce64fdd68ddfbc0c36aaa6b5924522b431442d5f
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-08-02 13:51:48 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -1,12 +1,1 @@
-import cloudscraper
-from bs4 import BeautifulSoup
-import re
-
-hijk = input("enter your link")
-scraper = cloudscraper.create_scraper(allow_brotli=False)
-lmno=scraper.get(hijk).text
-soup4=BeautifulSoup(lmno,'html.parser')
-mag=re.compile(r"magnet:\?xt=urn:btih:[a-zA-Z0-9]*")
-for pqrs in soup4.find_all('a',href=re.compile(r'magnet:\?xt*')):
-    tuvw =pqrs.get('href')
-    print (tuvw)
+.
