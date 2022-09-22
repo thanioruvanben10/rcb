5945c5e2d3223298937718891f2a2f359becb4c4
thanioruvanben10 <91356119+thanioruvanben10@users.noreply.github.com>
2022-09-22 13:59:29 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -4,11 +4,11 @@ import cloudscraper
 import concurrent.futures
 from bs4 import BeautifulSoup
 def expertlinks_scrape(url):
-    client = cloudscraper.create_scraper(allow_brotli=False)    
+    newclient = cloudscraper.create_scraper(allow_brotli=False)    
     h = {
     'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
     }
-    res = client.get(url, cookies={}, headers=h)
+    res = newclient.get(url, cookies={}, headers=h)
     url = re.findall('\&url=(.*?)"', res.text)[0]
     print(base64.b64decode(url).decode('utf-8'))
 
@@ -21,6 +21,7 @@ def atozcartoonist_bypasser(psa_url):
         for link in soup:
             try:
                 exit_gate = link.a.get("href")
+                print (exit_gate)
                 executor.submit(expertlinks_scrape, exit_gate)
             except Exception as e:
                 print(e)
