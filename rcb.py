adf000fd713d9b428125dfa6adace0eb33e19380
thanioruvanben10 <91356119+thanioruvanben10@users.noreply.github.com>
2022-09-22 15:10:39 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -1,5 +1,5 @@
 import base64
-import re
+import re,json
 import cloudscraper 
 import concurrent.futures
 from bs4 import BeautifulSoup
@@ -9,8 +9,9 @@ def expertlinks_scrape(url):
     'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
     }
     res = client.get(url, cookies={}, headers=h)
-    url = re.findall(r"value='*'",res.text)
-    print (url)
+    test = json.loads(res.text)
+    value = test['value']
+    return value
     
 def atozcartoonist_bypasser(psa_url):
     client = cloudscraper.create_scraper(allow_brotli=False)
