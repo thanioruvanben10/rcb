f4d4f26a176e630f01fee7ba258400d4a879269b
thanioruvanben10 <91356119+thanioruvanben10@users.noreply.github.com>
2022-09-22 13:54:04 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -1,15 +1,29 @@
-import requests,json
-video_id = "1000036883"
-cdn_url = "http://getcdn.hotstar.com/AVS/besc?action=GetCDN&asJson=Y&channel=TABLET&id=" + video_id + "&type=VOD"
-header = {
-    'Accept': '*/*',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Referer': 'https://hotstar.com/',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
-}
-response = requests.get(url=cdn_url, headers=header)
+/exec import base64
+import re
+import cloudscraper 
+import concurrent.futures
+from bs4  import BeautifulSoup
+def expertlinks_scrape(url):
+    client = cloudscraper.create_scraper(allow_brotli=False)    
+    h = {
+    'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+    }
+    res = client.get(url, cookies={}, headers=h)
+    url = re.findall('\&url=(.*?)"', res.text)[0]
+    print(base64.b64decode(url).decode('utf-8'))
 
-json_response = json.loads(response.text.encode('utf-8'))
-stream_url = json_response['resultObj']['src'].encode('utf-8')
-print(stream_url)
+def atozcartoonist_bypasser(psa_url):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    r = client.get(psa_url)
+    soup = BeautifulSoup(r.text, "html.parser").find_all(class_="gdlink")
+    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for link in soup:
+            try:
+                exit_gate = link.a.get("href")
+                executor.submit(expertlinks_scrape, exit_gate)
+            except Exception as e:
+                print(e)
+
+url = "https://themoviesboss.shop/tvshows/thai-cave-rescue-2022-season-1-all-episodes-donwload-hindi-multi-audio-nf-web-dl/"
+atozcartoonist_bypasser(url)
