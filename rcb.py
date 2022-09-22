e5d537d291623d7105f145c37bf09f7d2efd8c97
thanioruvanben10 <91356119+thanioruvanben10@users.noreply.github.com>
2022-09-22 14:37:44 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -2,24 +2,19 @@ import base64
 import re
 import cloudscraper 
 import concurrent.futures
-from bs4  import BeautifulSoup
-        
-
+from bs4 import BeautifulSoup
 def expertlinks_scrape(url):
     client = cloudscraper.create_scraper(allow_brotli=False)    
     h = {
     'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
     }
     res = client.get(url, cookies={}, headers=h)
-    print (res.text)
-    url = re.findall('\&url=(.*?)"', res.text)[0]
-    print(base64.b64decode(url).decode('utf-8'))
-
+    return res
+    
 def atozcartoonist_bypasser(psa_url):
     client = cloudscraper.create_scraper(allow_brotli=False)
     r = client.get(psa_url)
     soup = BeautifulSoup(r.text, "html.parser").find_all(class_="gdlink")
-    
     with concurrent.futures.ThreadPoolExecutor() as executor:
         for link in soup:
             try:
@@ -28,6 +23,5 @@ def atozcartoonist_bypasser(psa_url):
                 executor.submit(expertlinks_scrape, tuvw)
             except Exception as e:
                 print(e)
-
 x = "https://themoviesboss.shop/tvshows/thai-cave-rescue-2022-season-1-all-episodes-donwload-hindi-multi-audio-nf-web-dl/"
 y = atozcartoonist_bypasser(x)
