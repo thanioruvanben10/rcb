8abe06d58f6bbc7f5326dfbb9669388273752d62
thanioruvanben10 <91356119+thanioruvanben10@users.noreply.github.com>
2022-09-22 16:16:01 +0530

    Update rcb.py

--- rcb.py
+++ rcb.py
@@ -10,7 +10,7 @@ def expertlinks_scrape(url):
     'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
     }
     res = client.get(url, cookies={}, headers=h)
-    value = re.findall(r'value=[\w+]*',res.text)
+    value = re.findall(r'value=[\w+]*.',res.text)
     print(value)
 
 def atozcartoonist_bypasser(psa_url):
