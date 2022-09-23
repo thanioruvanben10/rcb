834cce8bb4dfc153e84a7091d7091199dccfcdfd
thanioruvanben10 <91356119+thanioruvanben10@users.noreply.github.com>
2022-09-23 11:07:28 +0530

    Update rbc.py

--- rbc.py
+++ rbc.py
@@ -9,9 +9,7 @@ def tmb_bypass(url):
         x = executor.submit(inbbotlist, url)
 def inbbotlist(aurl):
     scraper = cloudscraper.create_scraper()
-    h = {
-         'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-    }
+    h = {'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'}
     res = client.get(aurl, cookies={}, headers=h)
     value = re.findall(r'value=\"(.*?)\"',res.text)
     return res
