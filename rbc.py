42e2a20fea17c8ed1750d78f07cd419b059a27bd
thanioruvanben10 <91356119+thanioruvanben10@users.noreply.github.com>
2022-09-25 22:25:39 +0530

    Update rbc.py

--- rbc.py
+++ rbc.py
@@ -1,15 +1,45 @@
+import base64
 import re
 import json
 import cloudscraper 
+import requests
 import concurrent.futures
 from bs4 import BeautifulSoup
-url = "https://inbbotlist.com/?O6Aux00kmbwApbsCadfEeFlgiHnikVE5UTXNOV29kcXpkdmR2dzdHMGZvYTR6RWppZERlb3JJQWlyUGdSZGNoS0lKYU1hdFlFU25uK0dPUFJUOEJ6NQ=="
-def tmb_bypass(url):
+def expertlinks_scrape(url):
+    client = cloudscraper.create_scraper(allow_brotli=False)    
+    h = {
+    'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+    }
+    res = client.get(url, cookies={}, headers=h)
+    value = re.findall(r'value=\"(.*?)\"',res.text)
+    code = base64.b64decode(value[1]).decode('utf-8')
+    coderes = json.loads(code)
+    newurl = coderes['linkr']
+    if "inbbotlist" in newurl:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            xy = executor.submit(expertlinks_scrape_, newurl)
+
+def expertlinks_scrape_(url):
+    client = cloudscraper.create_scraper(allow_brotli=False)    
+    h = {
+    'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+    }
+    res = requests.get(url, cookies={}, headers=h)
+    value = re.findall(r'value=\"(.*?)\"',res.text)
+    result = base64.b64decode(value[0]).decode('utf-8')
+    print(result)
+
+def atozcartoonist_bypasser(psa_url):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    r = requests.get(psa_url)
+    soup = BeautifulSoup(r.text, "html.parser").find_all(class_="gdlink")
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        x = executor.submit(inbbotlist, url)
-def inbbotlist(aurl):
-    scraper = cloudscraper.create_scraper(allow_brotli=False)
-    h = {'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'}
-    res = scraper.get(url, cookies={}, headers=h)
-    print(res)
-tmb_bypass(url)
+        for link in soup:
+            try:
+                tuvw =link.get('href')
+                x = executor.submit(expertlinks_scrape, tuvw)
+            except Exception as e:
+                print(e)
+
+x = "https://themoviesboss.shop/tvshows/thai-cave-rescue-2022-season-1-all-episodes-donwload-hindi-multi-audio-nf-web-dl/"
+atozcartoonist_bypasser(x)
