579f83e47516b6366143980fe64fad8a84ebe334
thanioruvanben10 <91356119+thanioruvanben10@users.noreply.github.com>
2022-09-26 16:30:58 +0530

    Update rbc.py

--- rbc.py
+++ rbc.py
@@ -1,43 +1,39 @@
-import base64
+import time
+import requests 
 import re
-import json
-import cloudscraper
-import requests
-import concurrent.futures
-from bs4 import BeautifulSoup
-def expertlinks_scrape(url):
-    client = cloudscraper.create_scraper(allow_brotli=False)    
-    h = {
-    'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-    }
-    res = client.get(url, cookies={}, headers=h)
-    value = re.findall(r'value=\"(.*?)\"',res.text)
-    code = base64.b64decode(value[1]).decode('utf-8')
-    coderes = json.loads(code)
-    newurl = coderes['linkr']
-    #if "inbbotlist" in newurl:
-        #with concurrent.futures.ThreadPoolExecutor() as executor:
-            #xy = executor.submit(expertlinks_scrape_, newurl)
-    print(res.text)
+import cloudscraper 
+from bs4  import BeautifulSoup
 
-def expertlinks_scrape_(url):
-    client = cloudscraper.create_scraper(allow_brotli=False)    
-    h = {
-    'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-    }
-    res = requests.get(url, cookies={}, headers=h)
-    value = re.findall(r'value=\"(.*?)\"',res.text)
-    result = base64.b64decode(value[0]).decode('utf-8')
-    print(result)
 
-def atozcartoonist_bypasser(urlx):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-            try:
-                site = requests.get(urlx)
-                zipk = site.url
-                x = executor.submit(expertlinks_scrape,zipk)
-            except Exception as e:
-                print(e)
+def try2link_bypass(url):
+	client = cloudscraper.create_scraper(allow_brotli=False)
+	
+	url = url[:-1] if url[-1] == '/' else url
+	
+	params = (('d', int(time.time()) + (60 * 4)),)
+	r = client.get(url, params=params, headers= {'Referer': 'https://newforex.online/'})
+	
+	soup = BeautifulSoup(r.text, 'html.parser')
+	inputs = soup.find(id="go-link").find_all(name="input")
+	data = { input.get('name'): input.get('value') for input in inputs }	
+	time.sleep(7)
+	
+	headers = {'Host': 'try2link.com', 'X-Requested-With': 'XMLHttpRequest', 'Origin': 'https://try2link.com', 'Referer': url}
+	
+	bypassed_url = client.post('https://try2link.com/links/go', headers=headers,data=data)
+	return bypassed_url.json()["url"]
+		
 
-x = "https://gplinks.co/HwsEUBZ0"
-atozcartoonist_bypasser(x)
+def psa_bypasser(url):
+	client = cloudscraper.create_scraper(allow_brotli=False)	
+	h = {
+	'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+	}
+	res = client.get(url, cookies={}, headers=h)
+	print(res.text)
+			
+ 
+site = request.get("https://gplinks.co/HwsEUBZ0")
+urlz = site.url
+print (urlx)
+psa_bypasser(urlx)
