559084cb11d70b9b102cc35ae16de5d634945182
thanioruvanben10 <91356119+thanioruvanben10@users.noreply.github.com>
2022-10-25 14:10:34 +0530

    Update rbcc.py

--- rbcc.py
+++ rbcc.py
@@ -1,47 +1,21 @@
-import requests, json
-import os, sys
-
-OTPSendURL = "https://prod.media.jio.com/apis/common/v3/login/sendotp"
-OTPVerifyURL = "https://prod.media.jio.com/apis/common/v3/login/verifyotp"
-
-def login(mobile_number):
-    send = requests.post(url = OTPSendURL, headers = {
-    'authority': 'prod.media.jio.com',
-    'pragma': 'no-cache',
-    'cache-control': 'no-cache',
-    'origin': 'https://www.jiocinema.com',
-    'referer': 'https://www.jiocinema.com/',
-    },
-     data = '{"number":"+91' + mobile_number +'"}'
-    )
-    if 'success' in str(send.content):
-        OTP = input ('Enter OTP Received: ')
-        verify = requests.post(url = OTPVerifyURL, headers = {
-        'authority': 'prod.media.jio.com',
-        'pragma': 'no-cache',
-        'origin': 'https://www.jiocinema.com',
-        'referer': 'https://www.jiocinema.com/',
-        'deviceid': '1727391720'
-        },
-        data = '{"number":"+91' + mobile_number + '","otp":"' + OTP + '"}')
-        creds = json.loads(verify.content)
-        load_creds(creds)
-    else:
-        print ("Wrong/Unregistered Mobile Number (ensure there's no +91 or 0 in the beginning)")
-        sys.exit()
-
-def load_creds(creds):
-    try:
-        ssotoken = creds['ssoToken']
-        uniqueID = creds['uniqueId']
-    except KeyError:
-        print ("Wrong OTP, Try again!")
-        sys.exit()
-    Creds = {
-        "ssotoken" : ssotoken,
-        "uniqueID" : uniqueID
+import requests,json
+creds={"ssotoken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJ1bmlxdWUiOiI5MzVkMTNiNy1mNzdhLTRmNzktOTYxYS1jMzEwZmFiY2QzZGMiLCJ1c2VyVHlwZSI6IlJJTHBlcnNvbiIsImF1dGhMZXZlbCI6IjIwIiwiZGV2aWNlSWQiOm51bGwsImp0aSI6IjBhNTc1YjNiLTU0OWEtNDI2OC1iNGEzLTgxMjE0NDg1N2ZhZiIsImlhdCI6MTY2NjY4NTkwMn0.CZZuAd71PDCQlQBfPM6I-BK7hIx6TX-HWkIHCwXp6YsW9Z6eg46cEkcJDqjgy9UxJ1d_DpkqWQi14vDGQH2XqA", "uniqueID": "935d13b7-f77a-4f79-961a-c310fabcd3dc"}
+ssotoken = creds['ssotoken']
+uniqueID = creds['uniqueID']
+Request_URL = "https://prod.media.jio.com/apis/common/v3/playbackrights/get/"
+Meta_URL = "https://prod.media.jio.com/apis/common/v3/metamore/get/"
+def get_manifest(VideoID):
+    headers = {
+    'ssotoken': ssotoken,
+    'bitrates': 'true',
+    'os': 'Android',
+    'user-agent': 'JioOnDemand/1.5.2.1 (Linux;Android 4.4.2) Jio',
+    'content-type': 'application/json',
+    'accept': 'application/json, text/plain, */*',
     }
-    print(json.dumps(Creds))
+    response = requests.post(url = Request_URL + VideoID , data = '{"uniqueId":"' + uniqueID + '"}' , headers = headers)
+    return json.loads(response.text)
 
-M_No = input ('Enter Mobile Number: ')
-login (M_No)
+vidid = "003c2a70247511edbfd9c13b366470c9"
+manifest = get_manifest(vidid)
+print (manifest)
