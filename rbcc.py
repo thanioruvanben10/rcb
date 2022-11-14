c72c605324dd19f2b317f8563e86369cd71d153b
thanioruvanben10 <91356119+thanioruvanben10@users.noreply.github.com>
2022-11-14 17:49:46 +0530

    Update rbcc.py

--- rbcc.py
+++ rbcc.py
@@ -1,1 +1,69 @@
-.
+import requests, json
+import os, sys
+
+OTPSendURL = "https://prod.media.jio.com/apis/common/v3/login/sendotp"
+OTPVerifyURL = "https://prod.media.jio.com/apis/common/v3/login/verifyotp"
+
+def login(mobile_number):
+    send = requests.post(url = OTPSendURL, headers = {
+    'authority': 'prod.media.jio.com',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'origin': 'https://www.jiocinema.com',
+    'referer': 'https://www.jiocinema.com/',
+    },
+     data = '{"number":"+91' + mobile_number +'"}'
+    )
+    if 'success' in str(send.content):
+        OTP = input ('Enter OTP Received: ')
+        verify = requests.post(url = OTPVerifyURL, headers = {
+        'authority': 'prod.media.jio.com',
+        'pragma': 'no-cache',
+        'origin': 'https://www.jiocinema.com',
+        'referer': 'https://www.jiocinema.com/',
+        'deviceid': '1727391720'
+        },
+        data = '{"number":"+91' + mobile_number + '","otp":"' + OTP + '"}')
+        creds = json.loads(verify.content)
+        load_creds(creds)
+    else:
+        print ("Wrong/Unregistered Mobile Number (ensure there's no +91 or 0 in the beginning)")
+        sys.exit()
+
+def load_creds(creds):
+    try:
+        ssotoken = creds['ssoToken']
+        uniqueID = creds['uniqueId']
+    except KeyError:
+        print ("Wrong OTP, Try again!")
+        sys.exit()
+    Creds = {
+        "ssotoken" : ssotoken,
+        "uniqueID" : uniqueID
+    }
+    print(json.dumps(Creds))
+
+M_No = input ('Enter Mobile Number: ')
+login (M_No)
+
+Request_URL = "https://prod.media.jio.com/apis/common/v3/playbackrights/get/"
+Meta_URL = "https://prod.media.jio.com/apis/common/v3/metamore/get/"
+def get_manifest(VideoID):
+    headers = {
+    'ssotoken': ssotoken,
+    'bitrates': 'true',
+    'os': 'Android',
+    'user-agent': 'JioOnDemand/1.5.2.1 (Linux;Android 4.4.2) Jio',
+    'content-type': 'application/json',
+    'accept': 'application/json, text/plain, */*',
+    }
+    response = requests.post(url = Request_URL + VideoID , data = '{"uniqueId":"' + uniqueID + '"}' , headers = headers)
+    return json.loads(response.text)
+
+vidid = input("Enter VideoId : ")
+if vidid==""
+    vidid = "003c2a70247511edbfd9c13b366470c9"
+else:
+    pass
+manifest = get_manifest(vidid)
+print (manifest)
