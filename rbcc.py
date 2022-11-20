b1947c3cda1d74e6b602f99d30001feeaf344144
thanioruvanben10 <91356119+thanioruvanben10@users.noreply.github.com>
2022-11-20 08:43:31 +0530

    Update rbcc.py

--- rbcc.py
+++ rbcc.py
@@ -1,9 +1,3 @@
-import requests, json
-import os, sys
-
-OTPSendURL = "https://prod.media.jio.com/apis/common/v3/login/sendotp"
-OTPVerifyURL = "https://prod.media.jio.com/apis/common/v3/login/verifyotp"
-
 def login(mobile_number):
     send = requests.post(url = OTPSendURL, headers = {
     'authority': 'prod.media.jio.com',
@@ -25,46 +19,10 @@ def login(mobile_number):
         },
         data = '{"number":"+91' + mobile_number + '","otp":"' + OTP + '"}')
         creds = json.loads(verify.content)
-        load_creds(creds)
+        print (creds)
     else:
         print ("Wrong/Unregistered Mobile Number (ensure there's no +91 or 0 in the beginning)")
         sys.exit()
 
-def load_creds(creds):
-    global ssotoken,uniqueID
-    try:
-        ssotoken = creds['ssoToken']
-        uniqueID = creds['uniqueId']
-    except KeyError:
-        print ("Wrong OTP, Try again!")
-        sys.exit()
-    Creds = {
-        "ssotoken" : ssotoken,
-        "uniqueID" : uniqueID
-    }
-    print(json.dumps(Creds))
-
 M_No = input ('Enter Mobile Number: ')
 login (M_No)
-
-Request_URL = "https://prod.media.jio.com/apis/common/v3/playbackrights/get/"
-Meta_URL = "https://prod.media.jio.com/apis/common/v3/metamore/get/"
-def get_manifest(VideoID):
-    headers = {
-    'ssotoken': ssotoken,
-    'bitrates': 'true',
-    'os': 'Android',
-    'user-agent': 'JioOnDemand/1.5.2.1 (Linux;Android 4.4.2) Jio',
-    'content-type': 'application/json',
-    'accept': 'application/json, text/plain, */*',
-    }
-    response = requests.post(url = Request_URL + VideoID , data = '{"uniqueId":"' + uniqueID + '"}' , headers = headers)
-    return json.loads(response.text)
-
-vidid = input("Enter VideoId : ")
-if vidid=="":
-    vidid = "003c2a70247511edbfd9c13b366470c9"
-else:
-    pass
-manifest = get_manifest(vidid)
-print (manifest)
